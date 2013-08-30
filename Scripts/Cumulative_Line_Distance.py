256c8574519fccc6f7b52327f99c34458d4d3278
BJEBN <bjorn.burr.nyberg@gmail.com>
2013-08-30 14:48:28 +0930

    Update Cumulative_Line_Distance.py

--- Scripts/Cumulative_Line_Distance.py
+++ Scripts/Cumulative_Line_Distance.py
@@ -79,7 +79,7 @@ for enum,feature in enumerate(layer.getFeatures()):
         endx,endy = points[-1][0],points[-1][1]
         ID = feature[Groupby_Field]
         if ID not in Lengths:
-            G = edges(ID)
+            G = edges[ID]
             Source = G.nodes()[0]
             for n in range(2):
                 Length,Path = nx.single_source_dijkstra(G,Source,weight='weight')
