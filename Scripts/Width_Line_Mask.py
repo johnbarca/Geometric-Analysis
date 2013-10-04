e59179d0a14427d64b3f94caf16fb1a59e6f2067
BJEBN <bjorn.burr.nyberg@gmail.com>
2013-10-04 09:25:32 +0200

    Added New Width_Line_Mask

--- Scripts/Width_Line_Mask.py
+++ Scripts/Width_Line_Mask.py
@@ -25,10 +25,12 @@
 #==================================
 ##[SAFARI]=group
 ##Centerline=vector
+##Mask=vector
 ##Calculate_Width_By=field Centerline
-##Calculate_Distance_By_Optional=string
+##Calculate_Distance_By=field Centerline
 ##Custom_Distance_Field_Optional=string 
-##Distance=number 1000
+##Max_Width_Optional=string
+##Threshold=number 100
 ##Width=output vector
 
 #Algorithm body
@@ -41,36 +43,39 @@ from math import sqrt
 
 Lengths = {}
 layer = st.getobject(Centerline)
+layer2 = st.getobject(Mask)
 
 if layer.fieldNameIndex("Distance") == -1:
     layer.dataProvider().addAttributes([QgsField("Distance",QVariant.Double)])
 if layer.fieldNameIndex("RDistance") == -1:
     layer.dataProvider().addAttributes([QgsField("RDistance",QVariant.Double)])
-
+if layer.fieldNameIndex("Code") == -1:
+    layer.dataProvider().addAttributes([QgsField("Code",QVariant.Int)])
+if layer.fieldNameIndex("Width") == -1:
+    layer.dataProvider().addAttributes([QgsField("Width",QVariant.Double)])
 edges = {}
-
+feats = [f for f in layer2.getFeatures()]
 layer.startEditing()
 layer.commitChanges() #Force creation of fields mentioned above
 Total = layer.featureCount()
-if Calculate_Distance_By_Optional:
-	progress.setText('Calculating Edges')
-	for enum,feature in enumerate(layer.getFeatures()):
-	    progress.setPercentage(int((100 * enum)/Total))
-	    try:
-		points = feature.geometry().asPolyline()
-		pnts1 = points[0][0],points[0][1]
-		pnts2 = points[-1][0],points[-1][1]
-		if Custom_Distance_Field_Optional:
-		    Length = float(feature[Custom_Distance_Field_Optional])
-		else:
-		    Length = feature.geometry().length()
-		ID = feature[Calculate_Distance_By_Optional]
-		if ID in edges:
-		    edges[ID].append((pnts1,pnts2,Length))
-		else:
-		    edges[ID] = [(pnts1,pnts2,Length)]
-	    except Exception:
-		continue ##Possible Collapsed Polyline?
+progress.setText('Calculating Edges')
+for enum,feature in enumerate(layer.getFeatures()):
+    progress.setPercentage(int((100 * enum)/Total))
+    try:
+        points = feature.geometry().asPolyline()
+        pnts1 = points[0][0],points[0][1]
+        pnts2 = points[-1][0],points[-1][1]
+        if Custom_Distance_Field_Optional:
+            Length = float(feature[Custom_Distance_Field_Optional])
+        else:
+            Length = feature.geometry().length()
+        ID = feature[Calculate_Distance_By]
+        if ID in edges:
+            edges[ID].append((pnts1,pnts2,Length))
+        else:
+            edges[ID] = [(pnts1,pnts2,Length)]
+    except Exception:
+        continue ##Possible Collapsed Polyline?
 
 fields= layer.pendingFields()
 
@@ -97,7 +102,13 @@ for enum,feature in enumerate(layer.getFeatures()):
 		endx,endy = pnt[-1][0],pnt[-1][1]
 		m = ((starty - endy)/(startx - endx)) #Slope
 		nr = -1*((startx - endx)/(starty - endy))#Negative Reciprocal
-			
+		FID = feature[Calculate_Distance_By]	
+		inter = feats[FID]
+		if Max_Width_Optional:
+			Distance = eval(Max_Width_Optional)
+		else:
+			Distance = inter.geometry().boundingBox().width()/2
+
 		if starty==endy or startx==endx: #if horizontal or vertical
 		    if starty == endy:
 			y1 = midx + Distance
@@ -143,31 +154,78 @@ for enum,feature in enumerate(layer.getFeatures()):
 			if m < -1:
 			    y2 = nr*(-Distance) + midy
 			    x2 = midx - Distance
+				
+		geom1 = QgsGeometry.fromPolyline([QgsPoint(x1,y1),QgsPoint(midx,midy)])
+		geom2 = QgsGeometry.fromPolyline([QgsPoint(midx,midy),QgsPoint(x2,y2)])		
+		data = geom1.intersection(inter.geometry())
 
-		geom = QgsGeometry.fromPolyline([QgsPoint(x1,y1),QgsPoint(midx,midy),QgsPoint(x2,y2)]) #Create line
+		if data.isMultipart(): #multiple intersections
+			Length = Distance
+			values = data.asMultiPoint()
+			for x,y in values:
+				dx = midx - x
+				dy = midy - y
+				curLength = sqrt((dx**2)+(dy**2))
+				if curLength < Length:
+					x1,y1 = x,y
+					Length = curLength
+			
+		else: #single intersection
+			x1,y1 = data.asPoint()
+			if (x1,y1) == (0,0): #Line does not intersect - Max Distance too low?
+				continue
+
+		
+		data = geom2.intersection(inter.geometry())
+		if data.isMultipart(): #multiple intersections
+			Length = Distance
+			values = data.asMultiPoint()
+			for x,y in values:
+				dx = midx - x
+				dy = midy - y
+				curLength = sqrt((dx**2)+(dy**2))
+				if curLength < Length:
+					x2,y2 = x,y
+					Length = curLength
+		else: #single intersection
+			x2,y2 = data.asPoint()
+			if (x2,y2) == (0,0): #Line does not intersect - Max Distance too low?
+				continue
+		
+		geom1 = QgsGeometry.fromPolyline([QgsPoint(x1,y1),QgsPoint(midx,midy)]) #Recalculate each segment
+		geom2 = QgsGeometry.fromPolyline([QgsPoint(midx,midy),QgsPoint(x2,y2)])	#Recalculate each segment	
+			
+		d = [geom1.length(),geom2.length()]
+		e = 100-((min(d)/max(d)) * 100)
+		if e < Threshold:
+			Code = 1
+		else:
+			Code = 0
+		geom = QgsGeometry.fromPolyline([QgsPoint(x1,y1),QgsPoint(midx,midy),QgsPoint(x2,y2)]) #Create line if its below threshold
 		fet.setGeometry(geom)
 		for field in fields:
 		    fet[field.name()] = feature[field.name()]
-		if Calculate_Distance_By_Optional:
-			FID = feature[Calculate_Distance_By_Optional]
-			FID2 = '%sB'%(FID)
-			if FID not in Lengths:
-			    G.add_weighted_edges_from(edges[FID])
-			    Source = G.nodes()[0]
-			    for n in range(2):
-				Length,Path = nx.single_source_dijkstra(G,Source,weight='weight')
-				Index = max(Length,key=Length.get)
-				Source = Path[Index][-1]
-			    Lengths[FID] = Length
-			    Length,Path = nx.single_source_dijkstra(G,Source,weight='weight')
-			    Lengths[FID2] = Length
+		
+		FID2 = '%sB'%(FID)
+		if FID not in Lengths:
+		    G.add_weighted_edges_from(edges[FID])
+		    Source = G.nodes()[0]
+		    for n in range(2):
+			Length,Path = nx.single_source_dijkstra(G,Source,weight='weight')
+			Index = max(Length,key=Length.get)
+			Source = Path[Index][-1]
+		    Lengths[FID] = Length
+		    Length,Path = nx.single_source_dijkstra(G,Source,weight='weight')
+		    Lengths[FID2] = Length
 
-			    G.clear()
-			try:
-				fet["Distance"]=Lengths[FID][(midx,midy)]   
-				fet["RDistance"]=Lengths[FID2][(midx,midy)] 
-			except Exception:
-				pass
+		    G.clear()
+		try:
+			fet["Width"]=geom.length()
+			fet["Code"]=Code
+			fet["Distance"]=Lengths[FID][(midx,midy)]   
+			fet["RDistance"]=Lengths[FID2][(midx,midy)] 
+		except Exception:
+			pass
 		
 		startx,starty = midx,midy
 		midx,midy = endx,endy
